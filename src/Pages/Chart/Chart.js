4b6370431cf66da2d334bf5764df1544b132b422
arjunaNR-gf <arjun042896@gmail.com>
2025-08-04 13:30:08 +0530

    added loader to chart

--- src/Pages/Chart/Chart.js
+++ src/Pages/Chart/Chart.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
-import { FB_API,Get_sync } from '../../ServiceForBackEnd/FireBaseConfig/FirebaseService';
+import { FB_API, Get_sync } from '../../ServiceForBackEnd/FireBaseConfig/FirebaseService';
 import Dropdown from '../../Component/Dropdown/Dropdown';
+import { useEffect, useState } from 'react';
+import LoaderNotificaiton from '../../Component/Notification/LoaderNotification';
 
 
 const otherSetting = {
@@ -16,105 +18,145 @@ const valueFormatter = (value) => `${value}`;
 
 export default function Chart() {
 
-    const [dataset,setDataSet] = React.useState([{amount:0,year:0,month:''}])
-
-    const monthDetails = [
-        "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"
-    ]
-        const [inputval, setInputVal] =
-            React.useState({year:'', month: monthDetails[new Date().getMonth()] })
-
-
-    const firebase_fetch= async ()=>{
-         const getData = await Get_sync(FB_API.daiilyspendInfo_Address);
-        
-                if (getData.exists()) {
-                    const tempDataD = Object.keys(getData.val()).map(key => ({
-                        ...getData.val()[key],
-                    }));
-        
-                    const temp = Object.keys(tempDataD[0]).map(key => ({
-                        ...tempDataD[0][key],
-                    }));
-
-              setDataSet( Object.values(
-  temp.filter(spend=> new Date(spend.dateOfSpend).getMonth()== monthDetails.indexOf(inputval.month) ).reduce((acc, curr) => {
-    const year = curr.dateOfSpend.slice(0, 4); // '2023' from '2023-01-02'
-
-    if (!acc[year]) {
-      acc[year] = { amount: 0,year:year ,month:' ' };
-    }
-
-    acc[year].amount += Number(curr.money);
-    acc[year].month= monthDetails[new Date(curr.dateOfSpend).getMonth()]
-    return acc;
-  }, {})
-))
-
- }
-               
+  const [dataset, setDataSet] = useState([])
+  const [Active, setActive] = useState(false)
+  const monthDetails = [
+    "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"
+  ]
+  const [inputval, setInputVal] =
+    useState({ year: '', month: monthDetails[new Date().getMonth()] })
 
 
-    }
+  const firebase_fetch = async (val) => {
+    let monthTemp = val==null?inputval.month:val;
+    setActive(true)
+    var getData = await Get_sync(FB_API.daiilyspendInfo_Address);
 
-    React.useEffect(()=>{
+    if (getData.exists()) {
+      var tempDataD = Object.keys(getData.val()).map(key => ({
+        ...getData.val()[key],
+      }));
 
-        firebase_fetch()
-    },[firebase_fetch])
+      var temp = Object.keys(tempDataD[0]).map(key => ({
+        ...tempDataD[0][key],
+      }));
 
+      let filteredData = temp.filter(spend =>
+       monthDetails[ new Date(spend.dateOfSpend).getMonth()] === monthTemp
+      );
 
-    const refresh = () =>{
-        setDataSet({
-            amount:0,year:0,month:''
-        })
-        setInputVal({year:'',month:''})
-    }
-
-
-    //set up input box value at end according to requirement
-    const InputHandler = (name, val) => {
-      setInputVal((prevState) => ({ [name]: val }))
+      if (filteredData.length > 0) {
      
-    }
-    //it will set the input type mean name 
-    const InputName = () => { return inputval.year !== '' ? 'year' : 'month' }
 
-    //it will set the input value based on ID and null value present
-    const InputSelVal = () => { return inputval.year !== '' ? inputval.year : inputval.month }
+        let reducedData = filteredData.reduce((acc, item) => {
+          let date = new Date(item.dateOfSpend);
+          let year = date.getFullYear();
+          let monthName = monthDetails[date.getMonth()];
+
+          if (!acc[year]) {
+            acc[year] = { amount: 0, year: year, month: monthName };
+          }
+
+          acc[year].amount += Number(item.money);
+          return acc;
+        }, {});
+
+        // Convert object to array
+        let finalData = Object.values(reducedData);
+
+        setTimeout(() => {
+          setDataSet(finalData);
+          setActive(false)
+        }, 1200);
+      }
+
+
+
+      // setTimeout(() => {
+      //   setDataSet(dataTemp)
+      //   console.log(dataset)
+
+      // }, 30)
+
+      // setDataSet(Object.values(
+      //   temp.filter(spend => new Date(spend.dateOfSpend).getMonth() == monthDetails.indexOf(inputval.month)).reduce((acc, curr) => {
+      //     const year = curr.dateOfSpend.slice(0, 4); // '2023' from '2023-01-02'
+
+      //     if (!acc[year]) {
+      //       acc[year] = { amount: 0, year: year, month: ' ' };
+      //     }
+
+      //     acc[year].amount += Number(curr.money);
+      //     acc[year].month = monthDetails[new Date(curr.dateOfSpend).getMonth()]
+      //     return acc;
+      //   }, {})
+      // ))
+      // setActive(true)
+    }
+  }
+
+  // useEffect(() => {
+  //   firebase_fetch()
+  // },[])
+
+
+  const refresh = () => {
+    setDataSet([])
+
+  }
+
+
+  //set up input box value at end according to requirement
+  const InputHandler = (name, val) => {
+    refresh()
+    setInputVal((prevState) => ({ [name]: val }))
+    firebase_fetch(val)
+  }
+  //it will set the input type mean name 
+  const InputName = () => { return inputval.year !== '' ? 'year' : 'month' }
+
+  //it will set the input value based on ID and null value present
+  const InputSelVal = () => { return inputval.year !== '' ? inputval.year : inputval.month }
 
   return (
     <>
-    <div className='char--main' style={{marginTop:'10px', border:'1px solid gray', padding:'10px'}}>
+
+
+      <div className='char--main' style={{ marginTop: '10px', border: '1px solid gray', padding: '10px', position: 'relative', height: '500px' }}>
+     { Active  && <LoaderNotificaiton type={'SC'} text={"Please wait for a second..."}/>}
         <span>
-            <h5>Select Month you want to compare!</h5>
+          <h5>Select Month you want to compare!</h5>
         </span>
-    <div className='drop--down--Chart'  style={{marginTop:'3px'}}>
-        <Dropdown size="medium" label="Select Month you want to compare!"  name="month"
-                            value={inputval.month}
-                            onClickmeth={InputHandler} dataAry={monthDetails} />
-    </div>
+        <div className='drop--down--Chart' style={{ marginTop: '3px' }}>
+          <Dropdown size="medium" label="Select Month you want to compare!" name="month"
+            value={inputval.month}
+            onClickmeth={InputHandler} dataAry={monthDetails} />
+        </div>
+        <br />
+        {dataset.length > 0 &&
+          <>
+            <BarChart
+              dataset={dataset}
+              xAxis={[
+                {
+                  scaleType: 'band',
+                  dataKey: 'year',
+                  valueFormatter: (year, context) =>
+                    context.location === 'tick'
+                      ? `${year}`
+                      : `${year}`,
+                  height: 40,
+                },
+              ]}
 
-    <br/>
-    <BarChart
-      dataset={dataset}
-      xAxis={[
-        {
-          scaleType: 'band',
-          dataKey: 'year',
-          valueFormatter: (year, context) =>
-            context.location === 'tick'
-              ? `${year}`
-              : `${year}`,
-          height: 40,
-        },
-      ]}
-     
-      series={[{ dataKey: 'amount', label: dataset[0].month, valueFormatter }]}
-      {...otherSetting}
-    />
+              series={[{ dataKey: 'amount', label: dataset[0].month, valueFormatter }]}
+              {...otherSetting}
+            />
+          </>
+        }
 
-    </div>
+      </div>
     </>
-    
+
   );
 }
